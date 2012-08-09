f0f05241a1575ca1737deba31135709c445c0afa
abiyasa <ilham.suhardi@gmail.com>
2012-08-09 18:15:15 +0200

    change eol to UNIX

--- app/models/GameObject.js
+++ app/models/GameObject.js
@@ -6,6 +6,8 @@ define([
 ], function (Backbone) {
     'use strict';
     var GameObject = Backbone.Model.extend({
+        DEBUG_TAG: 'GameObject',
+        
         defaults: {
             'name': 'GameObject',
             'energy': 100,
