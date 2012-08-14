eb8d5f8e98892663b425647f0ebe0f20df70b4c0
abiyasa <ilham.suhardi@gmail.com>
2012-08-14 15:29:15 +0200

    add render test on unit testing

--- test/jasmine/spec/test_screenmanager_01.js
+++ test/jasmine/spec/test_screenmanager_01.js
@@ -13,7 +13,7 @@ define([
     'use strict';
     jasmine.getFixtures().fixturesPath = 'spec/fixtures';
         
-    describe("ScreenManager", function () {
+    describe('ScreenManager', function () {
         beforeEach(function () {
             jasmine.getFixtures().set('<div role="main" id="main"></div>');
             this.screenManager = new ScreenManager({ el: $('#main') });
@@ -23,11 +23,11 @@ define([
             this.screenManager.clearView();
         });
 
-        it("starts with an empty view", function () {
+        it('starts with an empty view', function () {
             expect($('#main')).toBeEmpty();
         });
         
-        it("can show a simple view", function () {
+        it('can show a simple view', function () {
             var mainScreen = new Backbone.View({
                 id: 'simpleView',
                 className: 'main-view'
@@ -39,7 +39,7 @@ define([
             expect($('#main #simpleView')).toHaveClass('main-view');
         });
 
-        it("can show the same view twice", function () {
+        it('can show the same view twice', function () {
             var mainScreen = new Backbone.View({
                 id: 'simpleView',
                 className: 'main-view'
@@ -52,7 +52,7 @@ define([
             expect($('#main #simpleView')).toHaveClass('main-view');
         });
 
-        it("shows and removes a simple view", function () {
+        it('can show and remove a simple view', function () {
             var mainScreen = new Backbone.View({
                 id: 'simpleView',
                 className: 'main-view'
@@ -63,22 +63,35 @@ define([
             expect($('#main').children().length).toBe(0);
         });
         
-        it("shows 2 simple views", function () {
-            var mainScreen = new Backbone.View({
-                id: 'simpleView1',
-                className: 'main-view'
+        it('can show 2 simple views', function () {
+            var ShowTextView = Backbone.View.extend({
+                className: 'main-view',
+                
+                render: function () {
+                    $(this.el).html(this.options.message);
+                    return this;
+                }
             });
-            var infoScreen = new Backbone.View({
-                id: 'simpleView2',
-                className: 'main-view'
+            
+            var mainScreen = new ShowTextView({
+                id: 'simpleView1',
+                message: 'ohai'
             });
             this.screenManager.showView(mainScreen);
+            expect($('#main').children().length).toBe(1);
+            expect($('#main #simpleView1')).toHaveText('ohai');
+            
+            var infoScreen = new ShowTextView({
+                id: 'simpleView2',
+                message: 'hello'
+            });
             this.screenManager.showView(infoScreen);
             
             expect($('#main').children().length).toBe(1);
             expect($('#main')).not.toContain('div.#simpleView1');
             expect($('#main')).toContain('div.#simpleView2');
             expect($('#main #simpleView2')).toHaveClass('main-view');
-        });
+            expect($('#main #simpleView2')).toHaveText('hello');
+        });        
     });
 });
